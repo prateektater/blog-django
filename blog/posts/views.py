cb1388f029565c63f1614bc94d85a9fa94bd59a7
prateek tater <prateektater@gmail.com>
2018-07-07 15:52:05 -0400

    full singlr page post and reverse sorted post'

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Post
 # Create your views here.
 def home(request):
 
-	posts = Post.objects.order_by('pub_date')
+	posts = Post.objects.order_by('-pub_date')
 	return render(request, 'posts/home.html', {'posts': posts})
 
 def post_details(request, post_id):
-	return render(request, 'posts/posts_detail.html', {'post_id': post_id})
+#	post = Post.objects.get(pk = post_id)
+	post = get_object_or_404(Post, pk = post_id)
+
+	return render(request, 'posts/posts_detail.html', {'post': post})
